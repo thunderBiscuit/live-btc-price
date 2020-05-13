b38e6985082e4c16d390b56b98ce19aa49d5934a
thunderbiscuit <thunderB@protonmail.com>
2020-05-13 10:24:53 -0400

    clean up utils.js

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,7 +1,8 @@
-// fetch the current Kraken price (latest trade)
+// fetch the latest Kraken price (latest trade)
 // example: https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD
 
 // first call
+console.log("fetching latest Kraken USD price...");
 fetch("https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD")
   .then((response) => response.json())
   .then((data) => {
@@ -9,21 +10,22 @@ fetch("https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD")
 
     const price = data.result.XXBTZUSD.c[0];
     const pricerounded = Math.floor(price);
-    console.log("price rounded down: ", pricerounded);
+    console.log("latest Kraken price: ", pricerounded);
 
     document.getElementById("current-price").innerText = "$ " + pricerounded;
   });
 
 // query back the API every 5 seconds
 setInterval(function () {
+  console.log("fetching latest Kraken USD price...");
   fetch("https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      // console.log(data);
 
       const price = data.result.XXBTZUSD.c[0];
       const pricerounded = Math.floor(price);
-      console.log("price rounded down: ", pricerounded);
+      console.log("latest price: ", pricerounded);
 
       document.getElementById("current-price").innerText = "$ " + pricerounded;
     });
