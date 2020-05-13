29eb4322b5d78a9419010089803198b8401ebbc8
thunderbiscuit <thunderB@protonmail.com>
2020-05-13 08:45:17 -0400

    add continuous updating of ticker

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,6 +1,7 @@
 // fetch the current Kraken price (latest trade)
 // example: https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD
 
+// first call
 fetch("https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD")
   .then((response) => response.json())
   .then((data) => {
@@ -12,3 +13,18 @@ fetch("https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD")
 
     document.getElementById("current-price").innerText = "$ " + pricerounded;
   });
+
+// query back the API every 5 seconds
+setInterval(function () {
+  fetch("https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      const price = data.result.XXBTZUSD.c[0];
+      const pricerounded = Math.floor(price);
+      console.log("price rounded down: ", pricerounded);
+
+      document.getElementById("current-price").innerText = "$ " + pricerounded;
+    });
+}, 5000);
